0b7a1b4333f38de0424f2254a37df5110c7a38ad
Mellowball <78409735+Mellowball@users.noreply.github.com>
2023-12-18 16:21:42 +0900

    project

---
+++ enviroment_measure_inst/enviroment_measure_inst.ino
@@ -0,0 +1,118 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Adafruit_BMP280.h>    //Adafruit_BME280_Library by Adafruit
+#include <SoftwareSerial.h>
+
+const char* ssid = "iptimea704";        // 와이파이 이름
+const char* password = "rodemeng2019";  // 와이파이 비밀번호
+Adafruit_BMP280 bmp;                    // I2C
+SoftwareSerial pmsSerial(D6, D5);       // RX, TX
+unsigned char pms[32];
+int PM1_0, PM2_5, PM10;
+
+// 데이터베이스,와이파이 설정
+#include <FirebaseESP8266.h>    //Firebase ESP8266 Client by Mobizt
+#include <addons/TokenHelper.h>
+#include <addons/RTDBHelper.h>
+
+// 파이어베이스 개인정보
+#define DATABASE_URL "https://sensingtemhum-default-rtdb.firebaseio.com/"
+#define API_KEY "AIzaSyCkbQ0ocsNCKdr4qqfW3RBQihdPvE3-lPU"
+#define USER_EMAIL "kdoch1009@gmail.com"
+#define USER_PASSWORD "rodemeng2019"
+
+// 파이어베이스 데이터 객체 설정
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+unsigned long sendDataPrevMillis = 0;
+
+void setup() {
+  Serial.begin(115200);   // 시리얼 통신 시작
+  pmsSerial.begin(9600);  // PMS 시리얼 통신
+  delay(10);
+
+  // 와이파이 연결 시작
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  // 와이파이 연결 완료 시까지 대기
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // 와이파이 연결 완료 메시지 출력
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // BMP280 연결
+  while (!Serial) delay(100); 
+  Serial.println(F("BMP280 test"));
+  unsigned status;
+  status = bmp.begin();
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                     "try a different address!"));
+    Serial.print("SensorID was: 0x");
+    Serial.println(bmp.sensorID(), 16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+  // Firebase 설정
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+  config.api_key = API_KEY;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  config.database_url = DATABASE_URL;
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.reconnectNetwork(true);
+  fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+
+  // Firebase 시작
+  Firebase.begin(&config, &auth);
+  Firebase.setDoubleDigits(5);
+
+  Serial.println("Setup completed");
+}
+void loop() {
+  // BMP280 센서값
+  float temperature = bmp.readTemperature();
+  float pressure = bmp.readPressure() / 100;
+  float altitude = bmp.readAltitude(1013.25);
+
+  // PMS7003 센서값 읽기
+  if (pmsSerial.available() >= 32) {
+    for (int i = 0; i < 32; i++) {
+      pms[i] = pmsSerial.read();
+    }
+    PM1_0 = (pms[10] << 8) | pms[11];
+    PM2_5 = (pms[12] << 8) | pms[13];
+    PM10 = (pms[14] << 8) | pms[15];
+  }
+
+  // 센서값 Firebase에 전송 및 시리얼 출력
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0)) {
+    sendDataPrevMillis = millis();
+    Serial.printf("Push temperature... %s\n", Firebase.pushFloat(fbdo, F("/temperature"), temperature) ? String(temperature) : fbdo.errorReason().c_str());
+    Serial.printf("Push preesure... %s\n", Firebase.pushFloat(fbdo, F("/pressure"), pressure) ? String(pressure) : fbdo.errorReason().c_str());
+    Serial.printf("Push altitude... %s\n", Firebase.pushFloat(fbdo, F("/altitude"), altitude) ? String(altitude) : fbdo.errorReason().c_str());
+    Serial.printf("Push PM1_0... %s\n", Firebase.pushInt(fbdo, "/PM1_0", PM1_0) ? String(PM1_0) : fbdo.errorReason().c_str());
+    Serial.printf("Push PM2_5... %s\n", Firebase.pushInt(fbdo, "/PM2_5", PM2_5) ? String(PM2_5) : fbdo.errorReason().c_str());
+    Serial.printf("Push PM10... %s\n", Firebase.pushInt(fbdo, "/PM10", PM10) ? String(PM10) : fbdo.errorReason().c_str());
+    Serial.println();
+  }
+}
---
+++ realtime_graph.py
@@ -0,0 +1,74 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import matplotlib.pyplot as plt
+
+# Firebase 서비스 계정 키 파일 경로
+cred = credentials.Certificate("sensingtemhum-firebase-adminsdk-tsmj6-2218d26a4d.json")
+
+# Firebase 앱 초기화 실시간버전
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://sensingtemhum-default-rtdb.firebaseio.com/'
+})
+
+# 변수명과 참조를 매핑
+ref_dict = {
+    'temperature': db.reference('temperature'),
+    'pressure': db.reference('pressure'),
+    'altitude': db.reference('altitude'),
+    'PM1_0': db.reference('PM1_0'),
+    'PM2_5': db.reference('PM2_5'),
+    'PM10': db.reference('PM10'),
+}
+
+# 변수명과 단위를 매핑
+unit_dict = {
+    'temperature': '°C',
+    'pressure': 'hPa',
+    'altitude': 'm',
+    'PM1_0': 'µg/m³',
+    'PM2_5': 'µg/m³',
+    'PM10': 'µg/m³',
+}
+
+# 각 데이터별 키값 리스트 참조
+keys_list = list(ref_dict.keys())
+# 그래프 제목 폰트
+title_font = {
+    'fontsize': 10,
+    'fontweight': 'bold'
+}
+# 그래프 창 생성
+plt.figure(figsize=(10, 15))
+
+# 각 변수에 대한 그래프 생성
+lines = []
+for i, (key, ref) in enumerate(ref_dict.items()):
+    lines.append(plt.subplot(len(ref_dict)//2, 2, i+1).plot([], [], label=key)[0])
+    plt.title(keys_list[i], fontdict=title_font, loc='left')
+    plt.ylabel(unit_dict[key])  # y축 라벨 추가
+    # plt.subplot(len(ref_dict)//2, 2, i+1).legend()
+
+# 그래프 창 전체화면
+manager = plt.get_current_fig_manager()
+manager.full_screen_toggle()
+plt.ion()
+plt.show()
+
+# 현재 열려 있는 그래프 창이 하나라도 있으면 True
+while plt.get_fignums():
+    # 각 변수의 데이터를 가져옴
+    data = []
+    for ref in ref_dict.values():
+        all_data = [float(value) for value in ref.get().values()]
+        data.append(all_data[-17200:])  # 마지막 17200개의 데이터만 선택
+
+    # 그래프 업데이트
+    for i in range(len(ref_dict)):
+        lines[i].set_ydata(data[i])
+        lines[i].set_xdata(range(len(data[i])))
+        plt.subplot(len(ref_dict)//2, 2, i+1).relim()
+        plt.subplot(len(ref_dict)//2, 2, i+1).autoscale_view()
+
+    plt.draw()
+    plt.pause(1)
